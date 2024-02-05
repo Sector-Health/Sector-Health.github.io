514ca491a6405b38880604a39b6f42285d0d018c
Jose Caballero <caballero.jimenez.jose@gmail.com>
2024-02-05 01:21:20 +0000

    update script url * updates hospital var name

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -715,7 +715,7 @@ $("#contactform").on("submit", function(e) {
 
   else {
     $.ajax({
-      url:"https://script.google.com/macros/s/AKfycbwj2to-f209j6kz6VVV40oP-EJg_KK7xcfMWG6R4GZ5prCQeWdHE0aZrsCl0fc68vIAQQ/exec",
+      url:"https://script.google.com/macros/s/AKfycbyZD4Bbc2qhItPsZG-twnR7noayuWvHAlfnWvVcAuwtjxdJJp7CmBJ9NgzzP3vtoDI4MQ/exec",
       data:$(this).serialize(),
       type:"POST",
       success:function(data){
